c29036cedbed75b58c7b249124cbeeadcad59492
MinityD <dispusplsk@gmail.com>
2026-02-28 15:23:08 -0300

    Timber.lua
    Signed-off-by: MinityD <dispusplsk@gmail.com>

--- Timber.lua
+++ Timber.lua
@@ -1,1 +1,1 @@
-https://api.jnkie.com/api/v1/luascripts/public/e9bd367ee4969548c2b5ea91e3ec093f204c7409784eda9815791708df222814/download
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e9bd367ee4969548c2b5ea91e3ec093f204c7409784eda9815791708df222814/download"))()
