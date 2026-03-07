baf1529dc1e850c8f5e548ff7e675cb9cbf6034f
MinityD <dispusplsk@gmail.com>
2026-03-07 09:10:43 -0300

    Update script URL in Timber.lua
    Signed-off-by: MinityD <dispusplsk@gmail.com>

--- Timber.lua
+++ Timber.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/vcWEyLuC/raw"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a8a1243883703313802f72d0f974b86047618a794649a839172d993204675a9e/download"))()
